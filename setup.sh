c7b8d190d7110a0419b5534f963c4cc8927755f9
Chris Lapa <chris@lapa.com.au>
2016-12-16 05:52:45 +0000

    setup: add basic setup script for dependancy installation.
    Signed-off-by: Chris Lapa <chris@lapa.com.au>

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Installing OpenCV"
+apt-get install python-opencv || exit 1
+
+echo "Installing Pip"
+apt-get install python-pip || exit 1
+
+echo "Installing Pyron4"
+pip install Pyro4 || exit 1
+
+echo "Installing RPi.GPIO"
+pip install RPi.GPIO || exit 1
+
