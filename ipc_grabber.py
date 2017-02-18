f8c9a8da087ba33fbf8f20b87b5448a30cd3e0d9
Chris Lapa <chris@lapa.com.au>
2017-02-18 23:15:31 +0000

    ipc_grabber: add support for jpeg and none formats sent.
    Signed-off-by: Chris Lapa <chris@lapa.com.au>

--- ipc_grabber.py
+++ ipc_grabber.py
@@ -12,6 +12,8 @@ from frame import Frame
 current_milli_time = lambda: int(round(time.time() * 1000))
 
 class IPCGrabber(object):
+    CompNone, CompLZ4, CompJPEG = range(3)
+
     def __init__(self, frame_queue):
         self.frame_queue = frame_queue
         self.memory = None
@@ -28,7 +30,7 @@ class IPCGrabber(object):
         self.stopped.wait(timeout)
 
     def read_frame(self):
-        mem_header_size = 6 * 4; # 6x uint32_t's
+        mem_header_size = 7 * 4; # 7x uint32_t's
 
         if self.memory == None:
             print "Allocating memory: {0}".format(mem_header_size)
@@ -36,7 +38,7 @@ class IPCGrabber(object):
             self.memory.bringup()
 
         mem_struct_data = self.memory.read(0, mem_header_size)
-        memory_size, xres, yres, slot1_offset, slot2_offset, current_slot = struct.unpack('@IIIIII', mem_struct_data)
+        memory_size, xres, yres, format, slot1_offset, slot2_offset, current_slot = struct.unpack('@IIIIIII', mem_struct_data)
 
         if self.memory.size != memory_size:
             print "Reallocating memory: {0}".format(memory_size)
@@ -48,17 +50,17 @@ class IPCGrabber(object):
         else:
             frame_offset = slot2_offset
 
-        frame_header_size = (2 * 4) + 1; # 2x uint32_t's + bool
+        frame_header_size = (2 * 4) + 1 + (3 * 1); # 2x uint32_t's + bool + 3x uint8_t packing
         frame_struct_data = self.memory.read(frame_offset, frame_header_size)
-        real_size, compressed_size, compressed = struct.unpack('@II?', frame_struct_data)
-        if compressed:
-            size = compressed_size
-        else:
+        real_size, compressed_size, compressed, _, _, _ = struct.unpack('@IIBBBB', frame_struct_data)
+        if compressed == IPCGrabber.CompNone:
             size = real_size
+        else:
+            size = compressed_size
 
         print "Slot: {0} | size: {1} | compressed: {2}".format(current_slot, size, compressed),
         frame = self.memory.read(frame_offset + frame_header_size, size)
-        return frame, xres, yres
+        return frame, xres, yres, compressed
 
     def on_read_frames(self):
         print "IPC Grabber starting"
@@ -84,14 +86,22 @@ class IPCGrabber(object):
                 last_timestamp = now_timestamp
                 fps = 1 / diff
 
-                data = self.read_frame()
+                data, xres, yres, compression = self.read_frame()
                 print " | FPS: {0:.2f}".format(fps)
                 sem_generate.release()
 
             if data != None:
                 try:
-                    self.frame_queue.put(Frame(data, "str_lz4", now_timestamp, fps, fps, xres, yres, True), block=True, timeout=0.001)
+                    str_format = "str_"
+                    if compression == IPCGrabber.CompNone:
+                        str_format += "none"
+                    elif compression == IPCGrabber.CompLZ4:
+                        str_format += "lz4"
+                    elif compression == IPCGrabber.CompJPEG:
+                        str_format += "jpeg"
+                    self.frame_queue.put(Frame(data, str_format, now_timestamp, fps, fps, xres, yres), block=True, timeout=0.001)
                 except Exception, e:
+                    print e
                     pass
 
         print "Grabber stopping"
